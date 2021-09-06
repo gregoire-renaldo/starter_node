2e90bb4cbb3828e2883a2d1faf574a8c7a5f19c4
gregoire renaldo <gregoire.renaldo@gmail.com>
2021-09-06 09:53:10 +0200

    push

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 // Mise en route Express
 const express = require('express');
 // Dot env to keep secrets !
-require('dotenv').config()
-
-const AppError = require('./utils/appError')
-const globalErrorHandler = require('./controllers/errorController')
+require('dotenv').config();
+// limit request
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp')
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// Cross-origin requete -- React Next to come
+// Global middleware
+// security http headers
+app.use(helmet())
+
+// limit number of requests
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 *1000,
+  message: 'Too many requests from this IP, please try again later'
+});
+app.use('/user', limiter)
+
+
+// Cross-origin request -- React Next to come
 const cors = require('cors');
 
 // db Mongo
@@ -22,19 +40,38 @@ mongoose.connect(process.env.DATABASE,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(cors());
-  // To parse get post update
-app.use(express.json())
+
+  // Body parserTo parse get post update, reading data from body into req.body
+app.use(express.json({
+  limit: '10kb'
+}));
+
+// data sanitization against NoSql query injection
+// filter query, params etc..
+app.use(mongoSanitize());
+// data sana against XSS
+app.use(xss());
+// prevent parameter pollution, clear up query string, + specify whitlist
+app.use(hpp({
+  // fields for a sort query to whitelistfor example, criterias of a boat ?
+  // creat a function for whitelist the fields ?
+  whitelist: ['duration', ]
+}));
+
 app.use(express.urlencoded({ extended: false }))
 
+// serving satic files
+app.use(express.static(__dirname + '/public'));
 // Setting the views with ejs
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 
 // '/' est la route racine
 app.get('/', function (req, res) {
   res.render('pages/home');
 });
 
+
+// Routes
 // renter boat
 
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,21 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    // convert in ms
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    // only https, but works only in production, so handled in the if
+    // secure: true,
+    // impossible to modify cookie in the browser
+    httpOnly: true
+  }
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  // store in cookie
+  res.cookie('jwt', token, cookieOptions );
+
+    // remove the password from the output
+  user.password = undefined
   res.status(statusCode).json({
     status: 'Success',
     token,
@@ -33,9 +48,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     lastname: req.body.lastname,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
-  });
+    passwordConfirm: req.body.passwordConfirm,
 
+  });
   createSendToken(newUser, 201, res)
 })
 
@@ -53,7 +68,6 @@ exports.login = catchAsync(async (req, res, next) => {
     if (!user || !(await user.correctPassword(password, user.password))) {
       return next(new AppError(' Incorrect email or password', 401))
     }
-
     // send token
     createSendToken(user, 200, res)
   }
